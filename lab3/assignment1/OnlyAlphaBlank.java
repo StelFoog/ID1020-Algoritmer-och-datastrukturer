11d54b30a52118f2feb3c391b2f9bbc7d9a31f38
samuel <samuel@familjenlarsson.eu>
2018-10-02 07:52:17 +0200

    update l3a1

--- lab3/assignment1/OnlyAlphaBlank.java
+++ lab3/assignment1/OnlyAlphaBlank.java
@@ -14,18 +14,20 @@
 package com.company;
 
 class OnlyAlphaBlank {
+    // checks if input is alphabetical (just a shorthand)
     private static boolean isAlpha(char c) {
         return Character.isLetter(c);
     }
 
+    // main for testing
     public static void main(String[] args) {
         char c;
-        while(!StdIn.isEmpty()) {
-            c = StdIn.readChar();
-            if(isAlpha(c) || c == ' ' || c == '\n')
-                System.out.print(c);
+        while(!StdIn.isEmpty()) {   // takes all chars until file is empty
+            c = StdIn.readChar();   // saves the next char
+            if(isAlpha(c) || c == ' ' || c == '\n')     // check if char is alphabetical, a space or a newline marker
+                System.out.print(c);    // adds to output file
             else
-                System.out.print(' ');
+                System.out.print(' ');  // else; adds space to output file
         }
     }
 }
